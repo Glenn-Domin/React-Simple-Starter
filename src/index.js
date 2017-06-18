8b6336ee0d145c818af229a61fa0bb706787d74c
Glenn-Domin <xaduurv@gmail.com>
2017-06-18 11:33:55 +1000

    Video Length + connection

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyA00wNubOZQocyFAp3kkp6KZasn1T70JMo';
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div className='george'>
         <SearchBar />
+        <VideoList videos={ this.state.videos } />
       </div>
     );
   }
